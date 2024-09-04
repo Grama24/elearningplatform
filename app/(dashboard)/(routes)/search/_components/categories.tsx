be909827e8132616b512d7de2e81cfb0171036cc
Grama24 <167801760+Grama24@users.noreply.github.com>
2024-09-04 13:31:46 +0300

    Initial commit

---
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import {
+  FcEngineering,
+  FcFilmReel,
+  FcMultipleDevices,
+  FcMusic,
+  FcCamera,
+  FcSalesPerformance,
+  FcSportsMode,
+} from "react-icons/fc";
+import { IconType } from "react-icons/lib";
+import { CategoryItem } from "./category-item";
+interface CategoriesProps {
+  items: Category[];
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+  Music: FcMusic,
+  Fitness: FcSportsMode,
+  "Computer Science": FcMultipleDevices,
+  Photography: FcCamera,
+  Filming: FcFilmReel,
+  "Electrical Engineering": FcEngineering,
+  Accounting: FcSalesPerformance,
+};
+
+export const Categories = ({ items }: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  );
+};
